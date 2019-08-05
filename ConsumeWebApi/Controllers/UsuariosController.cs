f12c999ce0ee8b8443d379b0548bb1d1100d53f4
Abigaíl Palacios <31462552+aspalacios@users.noreply.github.com>
2019-08-05 14:18:00 -0600

    Initial commit

---
+++ ConsumeWebApi/Controllers/UsuariosController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using ConsumeWebApi.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace ConsumeWebApi.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        string Baseurl = ".";
+
+        public async Task<ActionResult> Index()
+        {
+            List<Usuarios> EmpInfo = new List<Usuarios>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage Res = await client.GetAsync("api/usuarios");
+
+
+            }
+        }
+
+    }
+}
